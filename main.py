7b671b3f18da344a1f9e63988b1641fb944432ef
jorgegzz <A01571510@itesm.mx>
2021-10-15 14:50:10 -0500

    Max Token Prompt change

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def main():
         ctx = await client.get_context(message)
         if message.author == client.user:
             return
-        if len(message.content) / 3.5 > 11:
+        if len(message.content) > 14:
             engine = "davinci"
         else:
             engine = "curie"
