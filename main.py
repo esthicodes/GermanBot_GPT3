e89ffd03e62cccef2edb42a7cccdcc7ac0372336
Jorgegzz <A01571510@tec.mx>
2021-10-18 13:02:56 -0500

    help command update

--- main.py
+++ main.py
@@ -6,20 +6,42 @@ import OpenAI
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 PREFIX = "ger "
-client = commands.Bot(command_prefix=PREFIX, activity=discord.Game(name=PREFIX))
-
+client = commands.Bot(command_prefix=PREFIX, activity=discord.Game(name=f"{PREFIX}help"))
+client.remove_command("help")
 
 def main():
-    @client.command()
-    async def hello(ctx):
-        await ctx.send("Hi")
-
     @client.event
     async def on_ready():
         print("Bot has successfully logged in as: {}".format(client.user))
         print("Bot ID: {}\n".format(client.user.id))
 
     list_user = []
+
+    @client.command()
+    async def help(ctx):
+        embed=discord.Embed(
+                title="List of commands",
+                color=discord.Color.orange()
+
+        )
+        embed.add_field(
+            name="**ger ask**",
+            value="Ask any german related question. " \
+                  "Ex. `ger ask can you explain in english 'akkusativ' with an example?`",
+            inline=False
+        )
+        embed.add_field(
+            name="**ger correct**",
+            value="Corrects a sentence in German if the bot thinks it is incorrect. " \
+                  "Ex. `ger correct Der hamer klein ist`",
+            inline=False
+        )
+        embed.set_footer(
+            text="Warning: this bot is still being developed and you may encounter errors"
+        )
+        emoji = "\u2705"
+        await ctx.message.add_reaction(emoji)
+        await ctx.author.send(embed=embed)
 
     @client.command()
     async def ask(ctx, *, question):
@@ -37,7 +59,6 @@ def main():
             answer = OpenAI.ask(question)
             await ctx.send(answer)
 
-
     @client.command()
     async def correct(ctx, *, sentence):
         async with ctx.typing():
@@ -46,8 +67,6 @@ def main():
                 await ctx.send("I think there is no issue with your sentence")
             else:
                 await ctx.send(f"Maybe you should try saying: '{correction}'")
-
-
 
     @client.command()
     @commands.is_owner()
