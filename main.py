d21d7dbed45cb3cdbd1295e9c19b50ba03cebf32
Jorgegzz <A01571510@tec.mx>
2021-10-20 08:47:59 -0500

    DM disable

--- main.py
+++ main.py
@@ -9,19 +9,27 @@ PREFIX = "ger "
 client = commands.Bot(command_prefix=PREFIX, activity=discord.Game(name=f"{PREFIX}help"))
 client.remove_command("help")
 
+
 def main():
     @client.event
     async def on_ready():
         print("Bot has successfully logged in as: {}".format(client.user))
         print("Bot ID: {}\n".format(client.user.id))
 
-    list_user = []
+    @client.event
+    async def on_command_error(ctx, error):
+        if isinstance(error, commands.NoPrivateMessage):
+            await ctx.send(
+                embed=discord.Embed(
+                    description='**This command cannot be used in private messages.**',
+                    color=discord.Color.red()),
+            )
 
     @client.command()
     async def help(ctx):
-        embed=discord.Embed(
-                title="List of commands",
-                color=discord.Color.orange()
+        embed = discord.Embed(
+            title="List of commands",
+            color=discord.Color.orange()
 
         )
         embed.add_field(
@@ -43,30 +51,36 @@ def main():
         await ctx.message.add_reaction(emoji)
         await ctx.author.send(embed=embed)
 
-    @client.command()
-    async def ask(ctx, *, question):
-        words = len(question.split())
-        print(words)
-        if 2 >= words > 0:
-            OpenAI.ptype = "oneshot"
-        elif 6 >= words > 2:
-            OpenAI.ptype = "simple"
-        elif words > 6:
-            OpenAI.ptype = "complex"
-        async with ctx.typing():
-            # await message.channel.send('ping')
-            list_user.append(ctx.message.author.id)
-            answer = OpenAI.ask(question)
-            await ctx.send(answer)
+    list_user = []
 
     @client.command()
+    @commands.guild_only()
+    async def ask(ctx, *, question):
+        if ctx.message.channel.type != "dm":
+            words = len(question.split())
+            print(words)
+            if 2 >= words > 0:
+                OpenAI.ptype = "oneshot"
+            elif 6 >= words > 2:
+                OpenAI.ptype = "simple"
+            elif words > 6:
+                OpenAI.ptype = "complex"
+            async with ctx.typing():
+                # await message.channel.send('ping')
+                list_user.append(ctx.message.author.id)
+                answer = OpenAI.ask(question)
+                await ctx.send(answer)
+
+    @client.command()
+    @commands.guild_only()
     async def correct(ctx, *, sentence):
-        async with ctx.typing():
-            correction = OpenAI.correct(sentence)
-            if correction.strip() == sentence.strip():
-                await ctx.send("I think there is no issue with your sentence")
-            else:
-                await ctx.send(f"Maybe you should try saying: '{correction}'")
+        if ctx.message.channel.type != "dm":
+            async with ctx.typing():
+                correction = OpenAI.correct(sentence)
+                if correction.strip() == sentence.strip():
+                    await ctx.send("I think there is no issue with your sentence")
+                else:
+                    await ctx.send(f"Maybe you should try saying: '{correction}'")
 
     @client.command()
     @commands.is_owner()
